7884e6abb6a9e07794006004d09eebea3c10fee6
mochammadfariz <mochammadfariz11@gmail.com>
2019-06-21 00:22:27 +0700

    adding sign up activity & layout

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.login;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,8 +14,23 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
         getSupportActionBar().hide();
 
+
+    }
+
+    public void click (View v){
+        Intent intent;
+        switch (v.getId()){
+            case R.id.signin1:
+                intent = new Intent(MainActivity.this,signup.class);
+                break;
+            case R.id.signin2: // R.id.textView2
+                intent = new Intent(this, signup.class);
+                break;
+        }
+       startActivity(intent);
 
     }
 }
