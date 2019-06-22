87d2d3dfba121c54d0a6019ac9a1b305366195de
mochammadfariz <mochammadfariz11@gmail.com>
2019-06-22 22:32:30 +0700

    ganti name

--- app/src/main/java/com/example/login/signup.java
+++ app/src/main/java/com/example/login/signup.java
@@ -8,6 +8,6 @@ public class signup extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout);
+        setContentView(R.layout.activity_signup);
     }
 }
